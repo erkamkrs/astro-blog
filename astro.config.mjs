92f252195d22c16e8c9ab6675b3be685f06a043c
Erkam Kiris <erkamkiris@gmail.com>
2023-12-06 17:59:18 +0300

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from "@astrojs/netlify/functions";
+import vercelEdge from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
+  adapter: vercelEdge(),
   adapter: netlify()
 });
